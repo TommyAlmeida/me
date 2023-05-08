074717d3abec044a36eb31b28f7784bd8531a161
TommyAlmeida <tomasrsduarte@gmail.com>
2023-05-08 14:59:19 +0100

    Add logo to navbar

--- src/lib/components/Navabar.svelte
+++ src/lib/components/Navabar.svelte
@@ -4,7 +4,40 @@
       <div class="md:flex md:items-center md:gap-12">
         <a class="block text-black font-bold font-sans" href="/">
           <span class="sr-only">Home</span>
-          TRSD
+
+          <svg
+            class="h-8"
+            viewBox="0 0 1500 1705"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xml:space="preserve"
+            style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+          >
+            <g transform="matrix(1,0,0,1,-150,-227.813)">
+              <g transform="matrix(2.20588,0,0,2.11888,-1614.71,-1251.24)">
+                <g transform="matrix(1.0306,0,0,1,-32.6281,0)">
+                  <rect
+                    x="1001.97"
+                    y="698.034"
+                    width="194.062"
+                    height="804.375"
+                  />
+                </g>
+                <g transform="matrix(2.06119,0,0,0.253528,-1265.26,521.063)">
+                  <rect
+                    x="1001.97"
+                    y="698.034"
+                    width="194.062"
+                    height="804.375"
+                  />
+                </g>
+                <g transform="matrix(1,0,0,1,20,-0.982802)">
+                  <ellipse cx="1360" cy="800.983" rx="100" ry="100.983" />
+                </g>
+              </g>
+            </g>
+          </svg>
         </a>
       </div>
 
